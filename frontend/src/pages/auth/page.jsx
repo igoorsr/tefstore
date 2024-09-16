24e91cec7c427190e03ab1cf2608eb7d289f5b35
igordsrosa <igordossrosa@outlook.com>
2024-09-16 15:39:14 -0300

    inserindo autenticação

--- frontend/src/pages/auth/page.jsx
+++ frontend/src/pages/auth/page.jsx
@@ -1,3 +1,117 @@
+import { useEffect, useState } from "react";
+import { TextField, Button } from "@mui/material";
+import styles from "./page.module.css";
+import authServices from "../../services/auth";
+
 export default function Auth() {
-  return <h1>Auth</h1>;
+  const [formType, setFormType] = useState("login");
+  const [formData, setFormData] = useState(null);
+  const { login, signup, authLoading } = authServices();
+
+  const handleChangeFormType = () => {
+    setFormData(null);
+    if (formType === "login") {
+      setFormType("signup");
+    } else {
+      setFormType("login");
+    }
+  };
+
+  const handleFormDataChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    switch (formType) {
+      case "login":
+        login(formData);
+        break;
+      case "signup":
+        if (formData.password !== formData.confirmPassword) {
+          console.log("Passwords do not match");
+          return;
+        }
+        signup(formData);
+        break;
+    }
+  };
+
+  if (authLoading) {
+    return <h1>Loading</h1>;
+  }
+
+  if (formType === "login") {
+    return (
+      <div className={styles.authPageContainer}>
+        <h1 className={styles.title}>Login</h1>
+        <button className={styles.authButton} onClick={handleChangeFormType}>
+          Don't you have an account? Click here
+        </button>
+        <form onSubmit={handleSubmitForm}>
+          <TextField
+            required
+            label="Email"
+            type="email"
+            name="email"
+            onChange={handleFormDataChange}
+          />
+          <TextField
+            required
+            label="Password"
+            type="password"
+            name="password"
+            onChange={handleFormDataChange}
+          />
+          <Button type="submit">Login</Button>
+        </form>
+      </div>
+    );
+  }
+
+  if (formType === "signup") {
+    return (
+      <div className={styles.authPageContainer}>
+        <h1 className={styles.title}>Signup</h1>
+        <button className={styles.authButton} onClick={handleChangeFormType}>
+          Already have an account? Click here
+        </button>
+        <form onSubmit={handleSubmitForm}>
+          <TextField
+            required
+            label="Fullname"
+            type="fullname"
+            name="fullname"
+            onChange={handleFormDataChange}
+          />
+          <TextField
+            required
+            label="Email"
+            type="email"
+            name="email"
+            onChange={handleFormDataChange}
+          />
+          <TextField
+            required
+            label="Password"
+            type="password"
+            name="password"
+            onChange={handleFormDataChange}
+          />
+          <TextField
+            required
+            label="Confirm Password"
+            type="password"
+            name="confirmPassword"
+            onChange={handleFormDataChange}
+          />
+          <Button type="submit">Signup</Button>
+        </form>
+      </div>
+    );
+  }
 }
